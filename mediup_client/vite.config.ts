be1455ffc8872095b355140823c9ad02c2e3efb3
Areichao <annabel.rch31@gmail.com>
2024-07-14 04:17:32 -0400

    vite

--- mediup_client/vite.config.ts
+++ mediup_client/vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000,
+    host: '127.0.0.1',
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:8080/api',
@@ -16,5 +17,5 @@ export default defineConfig({
     }
   },
   preview: {
-    port: 10000
+    port: 5173
 }});
